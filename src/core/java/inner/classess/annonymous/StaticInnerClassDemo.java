20a4de43314cfa6df90083698b36204ebd845d1a
JeenaJoseph90 <45370725+JeenaJoseph90@users.noreply.github.com>
2020-02-27 13:53:03 +0400

    Demo added for static inner classes.

---
+++ src/core/java/inner/classess/annonymous/StaticInnerClassDemo.java
@@ -0,0 +1,33 @@
+package core.java.inner.classess.annonymous;
+
+public class StaticInnerClassDemo {
+
+	int x = 10;
+	static int y = 20;
+	
+	static class Inner {
+		
+		// Can declare static method inside including main method.
+		public static void main(String[] args) {
+			System.out.println("Inner main");
+		}
+		public static void m1() {
+			// System.out.println(x); // Not allowed to access non static members from static context.
+			System.out.println(y); // Can only access static members.
+			System.out.println("Inner class static method");
+		}
+		public void m2() {
+			// System.out.println(x); // Not allowed to access non static members from static inner class.
+			System.out.println(y); 
+			System.out.println("Inner class instance method");
+		}
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Outer Main");
+		StaticInnerClassDemo.Inner i = new StaticInnerClassDemo.Inner();
+		i.m2();
+		StaticInnerClassDemo.Inner.m1();
+	}
+	
+}
